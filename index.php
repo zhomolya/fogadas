87634070f59c5cbf17f548a52cf17e3ab131d9a4
Homolya Zoltán <122567154+zhomolya@users.noreply.github.com>
2023-10-12 22:55:38 +0200

    elso

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="./css/bootstrap.css">
+	
+    <script src="./js/bootstrap.js"></script>
+
+</head>
+<body>
+    yíxcxyíc
+</body>
+</html>
