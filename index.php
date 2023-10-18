453e31b9821787399487ff817dd0f73f82dcc4b2
Homolya Zoltán <122567154+zhomolya@users.noreply.github.com>
2023-10-18 23:56:20 +0200

    1

--- index.php
+++ index.php
@@ -65,12 +65,24 @@ if ($stmt->execute()) {
         $idopont=$_POST['idopont'];
         $penz=$_POST['penz'];
         $szorzo=$_POST['szorzo'];
-        
+        $tet=$_POST['tet'];
+
         if($szorzo>0){
         $ujtet=ceil($penz/$szorzo*100);
+        $sql = "UPDATE `buko` SET `befizet` = '$ujtet' WHERE `buko`.`ID` = $kit; "; 
+        echo($ujtet);}else{
+            $penz=$penz+$tet;
+            $sql="UPDATE `buko` SET `buko` = '$penz', `befizet` = '0' WHERE `buko`.`ID` = $kit;
+            ";
+        }
 
-        echo($ujtet);}
-
+        $stmt = $conn->prepare($sql);
+        //$stmt->bind_param("s", $valueToInsert); // "s" represents a string type
+        if ($stmt->execute()) {
+  //          echo "Data inserted successfully";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
 
 
 
@@ -95,7 +107,7 @@ if ($stmt->execute()) {
     if ($result->num_rows > 0) {
         echo "<table>"; 
         while ($row = $result->fetch_array()) {
-            echo "<tr><form method=post><td>" . $row[0] . "</td><td><input type=text name=csapat value=" . $row[1] . "></td><td><input type=text name=idopont value=" . $row[2] . "></td><td><input type=text name=penz value=" . $row[3] . "></td><td><input type=text name=szorzo value=" . $row[4] . "></td><td><input type=text name=tet value=" . $row[5] . "></td><td><input type=hidden name=kit value=" . $row[0] . "><input type=submit value=modosit name=modosit></td></form><td><form method=post><input type=hidden name=kit value=" . $row[0] . "><input type=submit name=torles value=torles></form></td></tr>";
+            echo "<tr><form method=post><td>" . $row[0] . "</td><td><input type=text name=csapat value='" . $row[1] . "'></td><td><input type=text name=idopont value='" . $row[2] . "'></td><td><input type=text name=penz value=" . $row[3] . "></td><td><input type=text name=szorzo value=" . $row[4] . "></td><td><input type=text name=tet value=" . $row[5] . "></td><td><input type=hidden name=kit value=" . $row[0] . "><input type=submit value=modosit name=modosit></td></form><td><form method=post><input type=hidden name=kit value=" . $row[0] . "><input type=submit name=torles value=torles></form></td></tr>";
         }
     
         echo "</table>";
